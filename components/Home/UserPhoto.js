545af82e9cdc88268df9f3a20bad409880eae61e
Harsh Patel <harsh.coderc@gmail.com>
2021-05-26 22:39:47 +0530

    Added Photo from external source

--- components/Home/UserPhoto.js
+++ components/Home/UserPhoto.js
@@ -1,16 +1,22 @@
 import Image from "next/image";
-import styles from "../../styles/Home.module.scss";
+import styles from "../../styles/Home/Home.module.scss";
+
+const myLoader = () => {
+    return `https://coderc-portfolio.vercel.app/_next/image?url=%2Fprofile_image.png&w=256&q=75`;
+};
 
 const UserPhoto = () => {
     return (
         <div>
             <span className={styles.logo}>
                 <Image
+                    loader={myLoader}
                     src="/profile_image.png"
                     alt="Harsh Patel"
                     width={150}
                     height={150}
                     priority={true}
+                    quality={100}
                 />
             </span>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
+import { Container } from "reactstrap";
 import UserData from "../components/Home/UserData";
 import UserPhoto from "../components/Home/UserPhoto";
-import styles from "../styles/Home.module.scss";
+import styles from "../styles/Home/Home.module.scss";
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <Container className={styles.container}>
             <UserPhoto />
             <UserData />
-        </div>
+        </Container>
     );
 }
