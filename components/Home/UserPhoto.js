dd8d6f0187cdb8610d4ec0c4021c58727b47ca56
Harsh Patel <harsh.coderc@gmail.com>
2021-05-27 09:10:15 +0530

    Changed Image

--- components/Home/UserPhoto.js
+++ components/Home/UserPhoto.js
@@ -1,16 +1,11 @@
 import Image from "next/image";
 import styles from "../../styles/Home/Home.module.scss";
 
-const myLoader = ({ src }) => {
-    return `https://coderc-portfolio.vercel.app/${src}&w=256&q=75`;
-};
-
 const UserPhoto = () => {
     return (
         <div className={styles.UserPhotoContainer}>
             <Image
-                loader={myLoader}
-                src="_next/image?url=profile_image.png"
+                src="/harsh_profile_image.png"
                 alt="Harsh Patel"
                 width={150}
                 height={150}
